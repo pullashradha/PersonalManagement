8cfc5f35dbeb915a84dd8a92223072f4a1ee03d8
Shradha Pulla <pullashradha@gmail.com>
2016-07-23 08:39:58 -0600

    Create CRUD methods in Category.cs, pass all tests in CategoryTest.cs

--- Objects/Category.cs
+++ Objects/Category.cs
@@ -6,6 +6,153 @@ namespace PersonalManagement
 {
   public class Category
   {
-
+    private int _id;
+    private string _name;
+    public Category (string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName (string newName)
+    {
+      _name = newName;
+    }
+    public override bool Equals (System.Object otherCategory)
+    {
+      if (otherCategory is Category)
+      {
+        Category newCategory = (Category) otherCategory;
+        bool idEquality = (this.GetId() == newCategory.GetId());
+        bool nameEquality = (this.GetName() == newCategory.GetName());
+        return (idEquality && nameEquality);
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlDataReader rdr;
+      SqlCommand cmd = new SqlCommand ("INSERT INTO categories (name) OUTPUT INSERTED.id VALUES (@CategoryName);", conn);
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@CategoryName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+      rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static List<Category> GetAll()
+    {
+      List<Category> allCategories = new List<Category> {};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlDataReader rdr;
+      SqlCommand cmd = new SqlCommand ("SELECT * FROM categories;", conn);
+      rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        int categoryId = rdr.GetInt32(0);
+        string categoryName = rdr.GetString(1);
+        Category newCategory = new Category (categoryName, categoryId);
+        allCategories.Add(newCategory);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allCategories;
+    }
+    public static Category Find (int queryId)
+    {
+      List<Category> foundCategories = new List<Category> {};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlDataReader rdr;
+      SqlCommand cmd = new SqlCommand ("SELECT * FROM categories WHERE id = @QueryId;", conn);
+      SqlParameter idParameter = new SqlParameter();
+      idParameter.ParameterName = "@QueryId";
+      idParameter.Value = queryId;
+      cmd.Parameters.Add(idParameter);
+      rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        int categoryId = rdr.GetInt32(0);
+        string categoryName = rdr.GetString(1);
+        Category foundCategory = new Category (categoryName, categoryId);
+        foundCategories.Add(foundCategory);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundCategories[0];
+    }
+    public void Update ()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand ("UPDATE categories SET name = @NewCategoryName WHERE id = @CategoryId;", conn);
+      SqlParameter newNameParameter = new SqlParameter();
+      newNameParameter.ParameterName = "@NewCategoryName";
+      newNameParameter.Value = this.GetName();
+      SqlParameter idParameter = new SqlParameter();
+      idParameter.ParameterName = "@CategoryId";
+      idParameter.Value = this.GetId();
+      cmd.Parameters.Add(newNameParameter);
+      cmd.Parameters.Add(idParameter);
+      cmd.ExecuteNonQuery();
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public void DeleteOne()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand ("DELETE FROM categories WHERE id = @CategoryId;", conn);
+      SqlParameter idParameter = new SqlParameter();
+      idParameter.ParameterName = "@CategoryId";
+      idParameter.Value = this.GetId();
+      cmd.Parameters.Add(idParameter);
+      cmd.ExecuteNonQuery();
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand ("DELETE FROM categories;", conn);
+      cmd.ExecuteNonQuery();
+    }
   }
 }
