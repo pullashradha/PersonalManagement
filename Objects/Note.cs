d404207b4500a18421773bbf8d6ff7d9a0f5d33e
Shradha Pulla <pullashradha@gmail.com>
2016-07-23 08:49:41 -0600

    Create CRUD methods in Note.cs

--- Objects/Note.cs
+++ Objects/Note.cs
@@ -6,6 +6,174 @@ namespace PersonalManagement
 {
   public class Note
   {
-
+    private int _id;
+    private string _title;
+    private string _content;
+    public Note (string Title, string Content, int Id = 0)
+    {
+      _id = Id;
+      _title = Title;
+      _content = Content;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetTitle()
+    {
+      return _title;
+    }
+    public void SetTitle (string newTitle)
+    {
+      _title = newTitle;
+    }
+    public string GetContent()
+    {
+      return _content;
+    }
+    public void SetContent (string newContent)
+    {
+      _content = newContent;
+    }
+    public override bool Equals (System.Object otherNote)
+    {
+      if (otherNote is Note)
+      {
+        Note newNote = (Note) otherNote;
+        bool idEquality = (this.GetId() == newNote.GetId());
+        bool titleEquality = (this.GetTitle() == newNote.GetTitle());
+        bool contentEquality = (this.GetContent() == newNote.GetContent());
+        return (idEquality && titleEquality && contentEquality);
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlDataReader rdr;
+      SqlCommand cmd = new SqlCommand ("INSERT INTO notes (title, content) OUTPUT INSERTED.id VALUES (@NoteTitle, @NoteContent);", conn);
+      SqlParameter titleParameter = new SqlParameter();
+      titleParameter.ParameterName = "@NoteTitle";
+      titleParameter.Value = this.GetTitle();
+      SqlParameter contentParameter = new SqlParameter();
+      contentParameter.ParameterName = "@NoteContent";
+      contentParameter.Value = this.GetContent();
+      cmd.Parameters.Add(titleParameter);
+      cmd.Parameters.Add(contentParameter);
+      rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static List<Note> GetAll()
+    {
+      List<Note> allNotes = new List<Note> {};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlDataReader rdr;
+      SqlCommand cmd = new SqlCommand ("SELECT * FROM notes;", conn);
+      rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        int noteId = rdr.GetInt32(0);
+        string noteTitle = rdr.GetString(1);
+        string noteContent = rdr.GetString(2);
+        Note newNote = new Note (noteTitle, noteContent, noteId);
+        allNotes.Add(newNote);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allNotes;
+    }
+    public static Note Find (int queryId)
+    {
+      List<Note> foundNotes = new List<Note> {};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlDataReader rdr;
+      SqlCommand cmd = new SqlCommand ("SELECT * FROM notes WHERE id = @QueryId;", conn);
+      SqlParameter idParameter = new SqlParameter();
+      idParameter.ParameterName = "@QueryId";
+      idParameter.Value = queryId;
+      cmd.Parameters.Add(idParameter);
+      rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        int noteId = rdr.GetInt32(0);
+        string noteTitle = rdr.GetString(1);
+        string noteContent = rdr.GetString(2);
+        Note foundNote = new Note (noteTitle, noteContent, noteId);
+        foundNotes.Add(foundNote);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundNotes[0];
+    }
+    public void Update ()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand ("UPDATE notes SET title = @NewNoteTitle, content = @NewNoteContent WHERE id = @NoteId;", conn);
+      SqlParameter newTitleParameter = new SqlParameter();
+      newTitleParameter.ParameterName = "@NewNoteTitle";
+      newTitleParameter.Value = this.GetTitle();
+      SqlParameter newContentParameter = new SqlParameter();
+      newContentParameter.ParameterName = "@NewNoteContent";
+      newContentParameter.Value = this.GetContent();
+      SqlParameter idParameter = new SqlParameter();
+      idParameter.ParameterName = "@NoteId";
+      idParameter.Value = this.GetId();
+      cmd.Parameters.Add(newTitleParameter);
+      cmd.Parameters.Add(newContentParameter);
+      cmd.Parameters.Add(idParameter);
+      cmd.ExecuteNonQuery();
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public void DeleteOne()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand ("DELETE FROM notes WHERE id = @NoteId;", conn);
+      SqlParameter idParameter = new SqlParameter();
+      idParameter.ParameterName = "@NoteId";
+      idParameter.Value = this.GetId();
+      cmd.Parameters.Add(idParameter);
+      cmd.ExecuteNonQuery();
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand ("DELETE FROM notes;", conn);
+      cmd.ExecuteNonQuery();
+    }
   }
 }
--- Tests/NoteTest.cs
+++ Tests/NoteTest.cs
@@ -6,15 +6,69 @@ using Xunit;
 
 namespace PersonalManagement
 {
-  public class NoteTest : IDisposable
+  public class CategoryTest : IDisposable
   {
-    public NoteTest()
+    public CategoryTest()
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=personal_management_test;Integrated Security=SSPI;";
     }
+    [Fact]
+    public void Test_Empty_DatabaseEmptyAtFirst()
+    {
+      int dBValues = Category.GetAll().Count;
+      Assert.Equal(0, dBValues);
+    }
+    [Fact]
+    public void Test_Equals_EntriesMatch()
+    {
+      Category firstCategory = new Category ("Pet Chores");
+      Category secondCategory = new Category ("Pet Chores");
+      Assert.Equal(firstCategory, secondCategory);
+    }
+    [Fact]
+    public void Test_Save_SavesCategoryToDatabase()
+    {
+      Category newCategory = new Category ("Pet Chores");
+      newCategory.Save();
+      List<Category> manualCategoryList = new List<Category> {newCategory};
+      List<Category> methodCategoryList = Category.GetAll();
+      Assert.Equal(manualCategoryList, methodCategoryList);
+    }
+    [Fact]
+    public void Test_Find_ReturnsCategoryById()
+    {
+      Category newCategory = new Category ("Pet Chores");
+      newCategory.Save();
+      Category foundCategory = Category.Find(newCategory.GetId());
+      Assert.Equal(newCategory, foundCategory);
+    }
+    [Fact]
+    public void Test_Update_UpdatesCategoryEntry()
+    {
+      Category newCategory = new Category ("Pet Chores");
+      newCategory.Save();
+      newCategory.SetName("Pet Chores - Meghan");
+      newCategory.Update();
+      Category foundCategory = Category.Find(newCategory.GetId());
+      Category updatedCategory = new Category ("Pet Chores - Meghan");
+      Assert.Equal(newCategory.GetName(), foundCategory.GetName());
+      Assert.Equal(newCategory.GetName(), updatedCategory.GetName());
+    }
+    [Fact]
+    public void Test_DeleteOne_DeletesOneCategory()
+    {
+      Category firstCategory = new Category ("Pet Chores");
+      firstCategory.Save();
+      Category secondCategory = new Category ("Household Chores");
+      secondCategory.Save();
+      firstCategory.DeleteOne();
+      List<Category> reducedCategoryList = new List<Category> {secondCategory};
+      List<Category> deletedCategoryList = Category.GetAll();
+      Assert.Equal(reducedCategoryList, deletedCategoryList);
+    }
     public void Dispose()
     {
-      // Note.DeleteAll();
+      Category.DeleteAll();
     }
   }
 }
